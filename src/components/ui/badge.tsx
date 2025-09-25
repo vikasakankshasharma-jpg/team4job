0808bcd9e35417ab21558385966c0179e7426e24
Harshita Sharma <harshitasharmaa0212@gmail.com>
2025-09-25 17:47:34 +0000

    proceed

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
@@ -40,3 +41,5 @@ function Badge({ className, variant, ...props }: BadgeProps) {
 }
 
 export { Badge, badgeVariants }
+
+    
