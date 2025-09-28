3a34871c55b88d062cc52eb9ee3d8c5d48d6c1d6
Harshita Sharma <harshitasharmaa0212@gmail.com>
2025-09-28 14:19:58 +0000

    still facing same issue as i hover above the "Sign Up" option which is j

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -9,6 +9,7 @@ export const Logo = (props: SVGProps<SVGSVGElement>) => (
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
+    style={{ pointerEvents: 'none' }}
     {...props}
   >
     <path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z" />
