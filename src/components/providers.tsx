69d49efd8b4f5285f33be305ec4c1b4341c5f197
Harshita Sharma <harshitasharmaa0212@gmail.com>
2025-10-12 09:00:16 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,28 +1,12 @@
 
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { UserProvider } from "@/hooks/use-user";
 import { ThemeProvider } from "@/components/theme-provider";
 import { HelpProvider } from "@/hooks/use-help";
 import { GoogleMapsProvider } from "@/hooks/use-google-maps";
 import { FirebaseErrorListener } from "./FirebaseErrorListener";
-import { getFirebaseApp, FirebaseAppContext } from "@/lib/firebase/use-firebase-app";
-import { FirebaseApp } from 'firebase/app';
-
-function FirebaseAppContextProvider({ children }: { children: React.ReactNode }) {
-    const [app, setApp] = useState<FirebaseApp | null>(null);
-
-    useEffect(() => {
-        setApp(getFirebaseApp());
-    }, []);
-
-    return (
-        <FirebaseAppContext.Provider value={app}>
-            {children}
-        </FirebaseAppContext.Provider>
-    );
-}
 
 export function Providers({ children }: { children: React.ReactNode }) {
 
@@ -33,16 +17,14 @@ export function Providers({ children }: { children: React.ReactNode }) {
       enableSystem
       disableTransitionOnChange
     >
-      <FirebaseAppContextProvider>
-        <UserProvider>
+      <UserProvider>
           <HelpProvider>
             <GoogleMapsProvider>
               {children}
               <FirebaseErrorListener />
             </GoogleMapsProvider>
           </HelpProvider>
-        </UserProvider>
-      </FirebaseAppContextProvider>
+      </UserProvider>
     </ThemeProvider>
   );
 }
