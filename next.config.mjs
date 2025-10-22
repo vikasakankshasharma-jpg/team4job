f5ae429fa6c195f9c7920c9de0dc6f3a543821e1
Harshita Sharma <harshitasharmaa0212@gmail.com>
2025-10-22 21:16:22 +0000

    why i am not able to login with mock data? (_for element main_)

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {},
+  allowedDevOrigins: [
+    "https://9000-firebase-studio-1758725206340.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev",
+    "https://3003-firebase-studio-1758725206340.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev",
+  ],
+};
+
+export default nextConfig;
