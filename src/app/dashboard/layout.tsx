2eb2d210ca424bb1180e33ddda20a2c2135583ab
Harshita Sharma <harshitasharmaa0212@gmail.com>
2025-10-29 06:57:12 +0000

    check this (_for element main_)

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -18,7 +18,7 @@ export default function DashboardLayout({
           <DashboardSidebar />
           <div className="flex flex-1 flex-col sm:gap-4 sm:py-4 sm:pl-14">
             <Header />
-            <main className="flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+            <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 md:gap-8">
               {children}
             </main>
           </div>
