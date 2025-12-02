51da6a286f65be2e409eb24f374bc7da8ff8aefa
Harshita Sharma <harshitasharmaa0212@gmail.com>
2025-12-02 18:40:45 +0000

    why i am not able to use selector on page.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,9 +16,9 @@ export default function DashboardLayout({
       <SearchProvider>
         <div className="flex min-h-screen w-full flex-row bg-muted/40">
           <DashboardSidebar />
-          <div className="flex flex-1 flex-col sm:gap-4 sm:py-4 sm:pl-14">
+          <div className="grid flex-1 auto-rows-auto">
             <Header />
-            <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 md:gap-8">
+            <main className="grid items-start gap-4 p-4 sm:px-6 md:gap-8">
               {children}
             </main>
           </div>
