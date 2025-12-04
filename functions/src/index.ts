44042d2c50b0b1f32ce5a4123a41338faf2fcd5d
Harshita Sharma <harshitasharmaa0212@gmail.com>
2025-12-04 10:14:52 +0000

    check platform's codes for syntax error and make list of all errors and

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -190,7 +190,7 @@ export const onJobCompleted = functions.firestore
 
                     const monthYear = new Date().toLocaleString("default", { month: "long", year: "numeric" });
                     const history = installerData.installerProfile.reputationHistory || [];
-                    const monthIndex = history.findIndex((h) => h.month === monthYear);
+                    const monthIndex = history.findIndex((h: { month: string; }) => h.month === monthYear);
                     if (monthIndex > -1) history[monthIndex].points = newPoints;
                     else history.push({ month: monthYear, points: newPoints });
                     if (history.length > 12) history.shift();
@@ -524,3 +524,5 @@ export const onUserVerified = functions.firestore
     
 
       
+
+    
