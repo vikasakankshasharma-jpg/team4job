0a58d3ceb3e6b6efc8e2ed0c807a7cee15d2f7c3
Vikas Sharma <vikasakankshasharma@gmail.com>
2026-01-04 12:49:21 +0530

    fix(csp): whitelist googletagmanager to prevent blocking

--- next.config.ts
+++ next.config.ts
@@ -69,7 +69,7 @@ const nextConfig: NextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: https: blob:",
-              "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.cloudfunctions.net https://sdk.cashfree.com https://www.google-analytics.com https://*.sentry.io https://api.postalpincode.in",
+              "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.cloudfunctions.net https://sdk.cashfree.com https://www.google-analytics.com https://*.sentry.io https://api.postalpincode.in https://*.googletagmanager.com",
               "worker-src 'self' blob:",
               "frame-src 'self' https://sdk.cashfree.com https://maps.googleapis.com",
               "object-src 'none'",
