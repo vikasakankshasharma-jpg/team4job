ed6954ec2372510dfd441527fd3848c970660ff4
Vikas Sharma <vikasakankshasharma@gmail.com>
2026-01-12 00:55:46 +0530

    refactor: remove deprecated sentry.client.config.ts

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,19 +1,12 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://80804373cc5c48b160dc622c548d35cd@o4510628917805056.ingest.us.sentry.io/4510628926455808",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
-
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
 });
