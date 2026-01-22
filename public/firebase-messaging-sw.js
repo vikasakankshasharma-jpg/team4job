1cc826be6ea7c159dca8ff3dec24781aac07bc38
Vikas Sharma <vikasakankshasharma@gmail.com>
2026-01-23 01:40:11 +0530

    chore: Update CSP in next.config and hardcode firebase config in SW for static serving

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,12 +5,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyC0NaO2x65_mdXpe1S62l2bwoc3KZwjn3o',
+  authDomain: 'dodo-beta.firebaseapp.com',
+  projectId: 'dodo-beta',
+  storageBucket: 'dodo-beta.firebasestorage.app',
+  messagingSenderId: '912974459558',
+  appId: '1:912974459558:web:0d04a5c2d3373c28547a67',
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -19,7 +19,7 @@ const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  
+
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
