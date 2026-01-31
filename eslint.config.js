23706cb00ea2ca577da17c404239e9d21004db12
Vikas Sharma <vikasakankshasharma@gmail.com>
2026-02-01 00:56:27 +0530

    fix: resolve CI lint errors and ignore non-source dirs

--- eslint.config.js
+++ eslint.config.js
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+    {
+        ignores: [".next/**", "hooks/**", "node_modules/**"],
+    },
     ...compat.extends("next/core-web-vitals"),
 ];
 
